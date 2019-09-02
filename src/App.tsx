fa1b2107bd48abe943c66ae8f21d7649a8e74a48
Sam Roelants <roelants.sam@gmail.com>
2019-09-03 00:58:01 +0200

    Added Landing, Logo and LandingText components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <p>"Hello"</p>
-  );
 
+const App: React.FC = () => {
+  return <Landing />
 }
+
+const Landing: React.FunctionComponent = () => {
+  return (<div className="landing">
+    <Logo />
+    <LandingText/>
+  </div>
+         );
+}
+
+const Logo: React.FunctionComponent = () => (
+  <div className="landing__logo">
+    <div className="landing__logo__bar landing__logo__bar1"></div>
+    <div className="landing__logo__bar landing__logo__bar2"></div>
+    <div className="landing__logo__bar landing__logo__bar3"></div>
+  </div>)
+
+const LandingText: React.FunctionComponent = () => (
+    <div className="landing__landingtext">
+    <div className="landing__landingtext__welcome">Welcome to</div>
+    <div className="landing__landingtext__sorted">Sorted!</div>
+    <p className="landing__landingtext__body">{"Sorted is an interactive tool for visualizing popular sorting algorithms. Simply select an algorithm, the size of the set you want to sort, and the animation speed."}</p>
+    </div>
+  )
+ 
 
 export default App;
