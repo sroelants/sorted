ce1146fe2e4257a00fbc67ed605dfaa9947ca3e7
Sam Roelants <roelants.sam@gmail.com>
2019-09-03 11:37:53 +0200

    Added AlgorithmBar

---
+++ src/AlgorithmBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './AlgorithmBar.sass'
+
+const AlgorithmBar: React.FunctionComponent = () => {
+  return (
+    <div className="algorithmbar">
+      {"Algorithms: "}
+      <button className="algorithmbar__algorithm">Merge sort</button>
+      <button className="algorithmbar__algorithm">Heap sort</button>
+      <button className="algorithmbar__algorithm">Bubble sort</button>
+      <button className="algorithmbar__algorithm">Quick sort</button>
+      {"Size: "}
+      <input type="range" min="10" max="1000" />
+      {"Speed: "}
+      <input type="range" min="10" max="1000" />
+    </div>);
+}
+
+export default AlgorithmBar
