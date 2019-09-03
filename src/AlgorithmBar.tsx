814f25110bdbad7013d56b75b5b99491bd06973e
Sam Roelants <roelants.sam@gmail.com>
2019-09-03 12:01:42 +0200

    Styled the Algorithm bar

--- src/AlgorithmBar.tsx
+++ src/AlgorithmBar.tsx
@@ -4,15 +4,21 @@ import './AlgorithmBar.sass'
 const AlgorithmBar: React.FunctionComponent = () => {
   return (
     <div className="algorithmbar">
-      {"Algorithms: "}
-      <button className="algorithmbar__algorithm">Merge sort</button>
-      <button className="algorithmbar__algorithm">Heap sort</button>
-      <button className="algorithmbar__algorithm">Bubble sort</button>
-      <button className="algorithmbar__algorithm">Quick sort</button>
-      {"Size: "}
-      <input type="range" min="10" max="1000" />
-      {"Speed: "}
-      <input type="range" min="10" max="1000" />
+      <div className="algorithmbar__algorithms">
+        <div className="algorithmbar__label">Algorithms:</div>
+        <button className="algorithmbar__algorithm algorithmbar__algorithm--active">Merge sort</button>
+        <button className="algorithmbar__algorithm">Heap sort</button>
+        <button className="algorithmbar__algorithm">Bubble sort</button>
+        <button className="algorithmbar__algorithm">Quick sort</button>
+      </div>
+      <div className="algorithmbar__size">
+        <div className="algorithmbar__label">Size:</div>
+        <input type="range" min="10" max="1000" className="algorithmbar__input"/>
+      </div>
+      <div className="algorithmbar__speed">
+      <div className="algorithmbar__label">Speed:</div>
+      <input type="range" min="10" max="1000" className="algorithmbar__input"/>
+      </div>
     </div>);
 }
 
