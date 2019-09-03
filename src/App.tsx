fea61e0780eef067ecd9b9ce2bd12c5d061d5889
Sam Roelants <roelants.sam@gmail.com>
2019-09-03 11:38:56 +0200

    Inserted algorithm bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.sass';
+import Landing from './Landing'
+import AlgorithmBar from './AlgorithmBar'
+
 
 
 const App: React.FC = () => {
-  return <Landing />
-}
-
-const Landing: React.FunctionComponent = () => {
-  return (<div className="landing">
-    <Logo />
-    <LandingText/>
-  </div>
-         );
-}
-
-const Logo: React.FunctionComponent = () => (
-  <div className="landing__logo">
-    <div className="landing__logo__bar landing__logo__bar1"></div>
-    <div className="landing__logo__bar landing__logo__bar2"></div>
-    <div className="landing__logo__bar landing__logo__bar3"></div>
-  </div>)
-
-const LandingText: React.FunctionComponent = () => (
-    <div className="landing__landingtext">
-    <div className="landing__landingtext__welcome">Welcome to</div>
-    <div className="landing__landingtext__sorted">Sorted!</div>
-    <p className="landing__landingtext__body">{"Sorted is an interactive tool for visualizing popular sorting algorithms. Simply select an algorithm, the size of the set you want to sort, and the animation speed."}</p>
+  return (
+    <div className="main">
+      <AlgorithmBar />
+      <Landing />
     </div>
-  )
- 
+  );
+
+}
 
 export default App;
