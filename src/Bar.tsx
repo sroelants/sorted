7a033388567d287ba52be7f9f3d303b290ba55e8
Sam Roelants <roelants.sam@gmail.com>
2019-09-12 09:35:41 +0200

    Add 'active' and 'testing' props to Bar component.
    Added extra boolean flags to indicate the state of the bars.

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Bar.sass';
 
-const Bar: React.FC<BarProps> = ({ height, width }) => {
+const Bar: React.FC<BarProps> = ({ height, width, active, testing }) => {
   return <div style={{minHeight: 4*height + "px", width: 100*width + "%",
-    margin: "0 " + 50 * width+ "px"} } className="figure__bar"></div>
+    margin: "0 " + 50 * width+ "px"} } 
+              className={"figure__bar" + (active? " active": "") + (testing? " testing": "")  }></div>
 }
 
 export interface BarProps {
