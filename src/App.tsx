2eedd957ccea565cdd7b86b02b9746a41faa0abf
Sam Roelants <roelants.sam@gmail.com>
2019-09-14 16:15:15 +0200

    Turn App into stateful component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,60 @@ import React from 'react';
 import './App.sass';
 import Landing from './Landing'
 import Plot from './Plot'
-import AlgorithmBar from './AlgorithmBar'
+import {AlgorithmBar, AlgorithmBarProps} from './AlgorithmBar'
 
-const App: React.FC = () => {
+interface AppProps {
+}
+
+interface AppState {
+  numOfBars: number
+  algorithm: string
+  speed: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      numOfBars: 100,
+      algorithm: "bubblesort",
+      speed: 1
+    }
+    this.setSpeed = this.setSpeed.bind(this);
+    this.setNumOfBars = this.setNumOfBars.bind(this);
+    this.setAlgorithm = this.setAlgorithm.bind(this);
+  }
+
+  setSpeed(speed: number) {
+    this.setState({speed: speed});
+  }
+
+  setNumOfBars(numOfBars: number) {
+    this.setState({numOfBars: numOfBars});
+  }
+
+  setAlgorithm(algorithm: string) {
+    this.setState({algorithm: algorithm});
+  }
+
+  render() {
+    let ABProps: AlgorithmBarProps = {
+      setSpeed: this.setSpeed,
+      setAlgorithm: this.setAlgorithm,
+      setNumOfBars: this.setNumOfBars
+    }
+
   return (
     <div className="main">
-      <AlgorithmBar />
+      <AlgorithmBar {...ABProps} />
       <div className="content-wrapper">
-        <Plot numOfBars={50} />
+        <Plot numOfBars={this.state.numOfBars} 
+              speed={this.state.speed} 
+              algorithm={this.state.algorithm} />
       </div>
     </div>
-  );
-
+    );
+  }
 }
 
 export default App;
