d5fa125a47f8ec11d53afff8a34ca3966b20f098
Sam Roelants <roelants.sam@gmail.com>
2019-09-14 16:51:00 +0200

    Move sorting logic to App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.sass';
 import Landing from './Landing'
-import Plot from './Plot'
+import {Plot, PlotProps} from './Plot'
 import {AlgorithmBar, AlgorithmBarProps} from './AlgorithmBar'
+import {Action, bubbleSort, swap} from './sort'
 
 interface AppProps {
 }
@@ -11,6 +12,10 @@ interface AppState {
   numOfBars: number
   algorithm: string
   speed: number
+  active: number,
+  testing: number,
+  heights: number[],
+  currentlySorting: boolean,
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -19,11 +24,19 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       numOfBars: 100,
       algorithm: "bubblesort",
-      speed: 1
+      speed: 1,
+      active: -1,
+      testing: -1,
+      heights: Array(100)
+          .fill(0)
+          .map((x) => Math.floor(100*Math.random())),
+      currentlySorting: false
     }
     this.setSpeed = this.setSpeed.bind(this);
     this.setNumOfBars = this.setNumOfBars.bind(this);
     this.setAlgorithm = this.setAlgorithm.bind(this);
+    this.performSort = this.performSort.bind(this);
+    this.applySort = this.applySort.bind(this);
   }
 
   setSpeed(speed: number) {
@@ -38,6 +51,29 @@ class App extends React.Component<AppProps, AppState> {
     this.setState({algorithm: algorithm});
   }
 
+  applySort(actions: Action[]) {
+    let [ac, ...acs] = actions;
+    if (ac.action === "active") {
+      this.setState({active: ac.position});
+    }
+    else if (ac.action === "testing") {
+      this.setState({testing: ac.position});
+    }
+    else if (ac.action === "swap" ){
+      let heights = swap(this.state.heights, ...ac.swap);
+      this.setState({heights: heights});
+    }
+    if (acs.length > 0) {
+      setTimeout(() => this.applySort(acs), 100);
+    }
+  }
+
+  performSort() {
+    let actions = getSort("bubblesort", this.state.heights)
+    this.setState({currentlySorting: true})
+    this.applySort(actions);
+  }
+
   render() {
     let ABProps: AlgorithmBarProps = {
       setSpeed: this.setSpeed,
@@ -45,17 +81,29 @@ class App extends React.Component<AppProps, AppState> {
       setNumOfBars: this.setNumOfBars
     }
 
+    let plotProps: PlotProps = {
+      numOfBars: this.state.numOfBars,
+      heights: this.state.heights,
+      active: this.state.active,
+      testing: this.state.testing,
+      currentlySorting: this.state.currentlySorting,
+      startSort: this.performSort
+    }
+
   return (
     <div className="main">
       <AlgorithmBar {...ABProps} />
       <div className="content-wrapper">
-        <Plot numOfBars={this.state.numOfBars} 
-              speed={this.state.speed} 
-              algorithm={this.state.algorithm} />
+        <Plot {...plotProps} />
       </div>
     </div>
     );
   }
+}
+
+function getSort(alg: string, array: number[]): Action[] {
+  let arr = array.slice(0);
+  return bubbleSort(arr);
 }
 
 export default App;
--- src/Plot.tsx
+++ src/Plot.tsx
@@ -6,86 +6,34 @@ import './Plot.sass'
 import {Action, bubbleSort, swap} from './sort';
 
 export interface PlotProps {
-  numOfBars: number,
-  algorithm: string,
-  speed: number
-}
-
-export interface PlotState {
+  heights: number[]
   numOfBars: number,
   active: number,
   testing: number,
-  heights: number[],
   currentlySorting: boolean,
-  algorithm: string,
-  speed: number
+  startSort: () => void
 }
 
-class Plot extends React.Component<PlotProps, PlotState> {
-  constructor(props: PlotProps) {
-    super(props)
-    this.state = {
-      numOfBars: props.numOfBars,
-      speed: props.speed,
-      algorithm: props.algorithm,
-      active: -1,
-      testing: -1,
-      heights: Array(props.numOfBars)
-          .fill(0)
-          .map((x) => Math.floor(100*Math.random())),
-      currentlySorting: false
-    }
-    this.sort = this.sort.bind(this);
-    this.applySort = this.applySort.bind(this);
-  }
-
-  applySort(actions: Action[]) {
-    let [ac, ...acs] = actions;
-    if (ac.action === "active") {
-      this.setState({active: ac.position});
-    }
-    else if (ac.action === "testing") {
-      this.setState({testing: ac.position});
-    }
-    else if (ac.action === "swap" ){
-      let heights = swap(this.state.heights, ...ac.swap);
-      this.setState({heights: heights});
-    }
-    if (acs.length > 0) {
-      setTimeout(() => this.applySort(acs), 100);
-    }
-  }
-
-  sort() {
-    let actions = getSort("bubblesort", this.state.heights)
-    this.applySort(actions);
-    this.setState({currentlySorting: true})
-  }
-
-  render() { 
-    const bars: JSX.Element[] = this.state.heights.map((h, i) => {
+const Plot: React.FC<PlotProps>  = (props: PlotProps) => {
+    const bars: JSX.Element[] = props.heights.map((h, i) => {
       return <Bar 
         key={i} 
-        active={i === this.state.active} 
-        testing={i === this.state.testing} 
+        active={i === props.active} 
+        testing={i === props.testing} 
         height={h}
-        width={1/this.state.heights.length} />
+        width={1/props.heights.length} />
     });
-    return (
+
+  return (
     <div className="plot">
       <Figure bars={bars} />
-      <Caption numOfBars={this.props.numOfBars} 
-        startSort={this.sort}
-        currentlySorting={this.state.currentlySorting}
+      <Caption numOfBars={props.numOfBars} 
+        currentlySorting={props.currentlySorting}
+        startSort={props.startSort}
       />
     </div>
   )
-  }
 }
   
-function getSort(alg: string, array: number[]): Action[] {
-  let arr = array.slice(0);
-  return bubbleSort(arr);
-}
 
-export default Plot
+export {Plot}
