8d49bc14f0a58066848381763c5a0dcb2c6fa32f
Sam Roelants <roelants.sam@gmail.com>
2019-09-14 16:15:57 +0200

    Change Caption to stateless component.

--- src/Caption.tsx
+++ src/Caption.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Plot.sass';
 
-export interface captionProps {
+export interface CaptionProps {
   startSort: () => void;
   numOfBars: number
   currentlySorting: boolean
@@ -13,37 +13,25 @@ interface captionState {
   currentlySorting: boolean
 }
 
-class Caption extends React.Component<captionProps, captionState> {
-  constructor(props: captionProps) {
-    super(props)
-    this.state = {
-                  startSort: props.startSort,
-                  numOfBars: props.numOfBars,
-                  currentlySorting: props.currentlySorting
-                }
-  }
-
-
-    render() {
-      let sortButton: JSX.Element = this.state.currentlySorting ? 
+const Caption: React.FC<CaptionProps>  = 
+  ({startSort, numOfBars, currentlySorting}: CaptionProps) => {
+      let sortButton: JSX.Element = currentlySorting ? 
         (<button className="caption__sortbutton caption__sortbutton--disabled">
           {"Sorting..."}
          </button>) : 
         (<button className="caption__sortbutton" 
           onClick={() => {
-            this.setState({currentlySorting: true})
-            this.state.startSort()
+            startSort()
           }}>Sort!</button>);
       return (
       <div className="caption">
         <div style={{display: 'flex'}}>
-          <div className="caption__elements">{"Number of elements: " + this.state.numOfBars}</div>
+          <div className="caption__elements">{"Number of elements: " + numOfBars}</div>
           <div className="caption__steps">{"Number of steps: " + 0}</div>
         </div>
         {sortButton}
       </div>
       );
   }
-}
 
 export {Caption}
