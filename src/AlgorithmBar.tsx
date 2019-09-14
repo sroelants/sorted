14930027997a2483cce69193acab6e7b3c65649d
Sam Roelants <roelants.sam@gmail.com>
2019-09-14 16:13:29 +0200

    Add event handlers to sliders.

--- src/AlgorithmBar.tsx
+++ src/AlgorithmBar.tsx
@@ -29,13 +29,17 @@ export interface AlgorithmBarProps {
       </div>
       <div className="algorithmbar__size">
         <div className="algorithmbar__label">Size:</div>
-        <input type="range" min="10" max="1000" className="algorithmbar__input"/>
+        <input type="range" min="10" max="1000" step="10"
+          className="algorithmbar__input"
+          onChange={(ev) => setNumOfBars(ev.target.valueAsNumber)}/>
       </div>
       <div className="algorithmbar__speed">
       <div className="algorithmbar__label">Speed:</div>
-      <input type="range" min="10" max="1000" className="algorithmbar__input"/>
+      <input type="range" min="0.1" max="10" step="0.1"
+        className="algorithmbar__input"
+        onChange={(ev) => setSpeed(ev.target.valueAsNumber)}/>
       </div>
     </div>);
 }
 
-export default AlgorithmBar
+export {AlgorithmBar}
