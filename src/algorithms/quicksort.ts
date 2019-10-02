109a2849a43081f37bbccead5256cd91b634b848
Sam Roelants <roelants.sam@gmail.com>
2019-10-02 18:13:21 +0200

    Quicksort.

--- src/algorithms/quicksort.ts
+++ src/algorithms/quicksort.ts
@@ -6,39 +6,33 @@
 //    to the right.
 // 3. Repeat with both sublists.
 
+const range = (start: number, end: number) =>
+  Array.from({ length: end - start }, (v, k) => k + start);
+
 function* quickSortGenerator(
   array: number[],
   min: number = 0,
   max: number = array.length
 ): any {
-  console.log("---STARTING QUICKSORT.---");
-  console.log("Minimal index: ", min);
-  console.log("Maximal index: ", max - 1);
-
-  if (max - min <= 1) return { array: array, active: [] };
+  if (max - min <= 1) return { array: array, active: [min] };
   // Partition array¬
   let pivot = array[min];
   let less: number[] = [];
   let greater: number[] = [];
 
   for (let i = min + 1; i < max; i++) {
-    console.log("Pivot is ", pivot);
-    console.log("Array element is ", array[i]);
     if (array[i] < pivot) less.push(array[i]);
     else greater.push(array[i]);
-    console.log("Less is now: ", less);
-    console.log("Greater is now: ", greater);
-    console.log("Splicing into array: ", less.concat([pivot]).concat(greater));
-    console.log("Splicing at position: ", min);
-    console.log("Replacing ", i - min + 1, " elements.");
     array.splice(min, i - min + 1, ...less.concat([pivot]).concat(greater));
-    console.log("Yielding array: ", array);
-    yield { array: array, active: [] };
+    yield {
+      array: array,
+      active: [min + less.length],
+      testing: range(min, max)
+    };
   }
 
-  console.log("Recursively quicksorting left: ", less);
   yield* quickSortGenerator(array, min, min + less.length);
-  console.log("Recursively quicksorting right: ", greater);
   yield* quickSortGenerator(array, min + less.length + 1, max);
+  return { array: array };
 }
 export { quickSortGenerator };
