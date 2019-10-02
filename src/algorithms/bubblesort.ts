dd2ae8e558dbb2665c3307847b1b1f72d50b54c4
Sam Roelants <roelants.sam@gmail.com>
2019-10-02 18:10:57 +0200

    Tweak bubbleSort algorithm.
    Bubblesort no longer sweeps the entire array, but only the subarray that
    is not yet sorted.

--- src/algorithms/bubblesort.ts
+++ src/algorithms/bubblesort.ts
@@ -1,8 +1,8 @@
 import { isSorted, swap } from "../utils/util";
 
 function* bubbleSortGenerator(array: number[]): Generator {
-  while (!isSorted(array)) {
-    for (let i = 0; i < array.length; i++) {
+  for (let j = array.length; j > 0; j--) {
+    for (let i = 0; i < j - 1; i++) {
       yield { array: array, active: [i, i + 1] };
       if (array[i] > array[i + 1]) {
         array = swap(array, i, i + 1);
